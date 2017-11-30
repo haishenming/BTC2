500f458dea861b1e2332c763f4d335cc732fd395
xiangyufan <xiangyufan@juwang.cn>
2017-11-30 15:26:07 +0800

    tt

--- BTC2.py
+++ BTC2.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
---
+++ libs/okex.py
@@ -0,0 +1,41 @@
+'''
+Created on 2017年11月30日 下午2:59
+
+@author: xiangyufan@juwang.cn
+
+'''
+
+import requests
+from setting import future_index_url, future_ticker_url
+
+class OKEX:
+    """ 获取和操作BTC数据
+    """
+
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def get_future_index(self):
+        """
+        :return: 获取OKEX合约指数信息
+        """
+        data = {
+            "symbol": self.symbol
+        }
+
+        response = requests.get(future_index_url, params=data)
+
+        if response.status_code == 200:
+            rdata = response.json()
+
+            if rdata.get('error_code'):
+                return {}, rdata
+            else:
+                return rdata, {'result': True, 'error_code': 0}
+        else:
+            return {}, {'result': False, 'error_code': response.status_code}
+
+
+if __name__ == '__main__':
+    btc = OKEX(symbol='btcusd')
+    btc.get_future_index()
