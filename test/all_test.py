b83de15248bd9557d85c43448d1f7429a789c647
xiangyufan <xiangyufan@juwang.cn>
2017-12-04 15:36:16 +0800

    celery tt

--- test/all_test.py
+++ test/all_test.py
@@ -6,7 +6,20 @@ Created on 2017年11月30日 下午4:02
 '''
 
 import requests
+import time
 
-codes = requests.codes.ok
+print("正在生成字典")
+test_dic = {str(k):v for k in range(10000) for v in range(10000)}
 
-print(codes)
+start1 = time.time()
+for k, v in test_dic.items():
+    pass
+end1 = time.time() - start1
+print("end1:", end1)
+
+
+start2 = time.time()
+for k, v in test_dic.iteritems():
+    pass
+end2 = time.time() - start2
+print("end2", end2)
