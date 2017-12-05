a12a19010d5741fde6b1da873d9e1266948a96f0
xiangyufan <xiangyufan@juwang.cn>
2017-12-05 14:53:07 +0800

    添加数据表

--- celery_tasks/celery_conf.py
+++ celery_tasks/celery_conf.py
@@ -8,14 +8,14 @@ Created on 2017年12月04日 下午6:00
 
 from celery import Celery
 
-def init_conf():
+def init_conf(env="dev"):
     """ 初始化配置
     """
 
     my_celery = Celery("main_tasks", backend='redis://localhost',
              broker='redis://localhost')
 
-    my_celery.config_from_object('config.setting')
+    my_celery.config_from_object('config.setting_{}'.format(env))
 
     return my_celery
 
--- data/db_func.py
+++ data/db_func.py
@@ -4,3 +4,17 @@ Created on 2017年12月05日 下午12:41
 @author: xiangyufan@juwang.cn
 
 '''
+
+
+
+def merge_record(instance):
+    """ 存在则修改，不存在则插入, 要求传入的是 models中的类
+    """
+    session.merge(instance)
+    session.commit()
+    return instance
+
+
+def get_obex():
+    """ 获取obex数据
+    """
